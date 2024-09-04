80581a406ab06c8fb79be2980bbd53aa0d98dc0f
RyanRay <ryanraylaguna28@gmail.com>
2024-09-04 21:24:30 +0800

    Test Push Using Different Account

--- public/db_test.php
+++ public/db_test.php
@@ -6,7 +6,7 @@ $user = 'root'; // Change this if you have a different MySQL user
 $pass = ''; // Change this if your MySQL user has a password
 
 // Create a connection
-$mysqli = new mysqli($host, $user, $pass, $db);
+$mysqli = new mysqli($host, $user, $pass, $db); //test push
 
 // Check connection
 if ($mysqli->connect_error) {
